9821f1ed097a8cd58228526a93cd0e0ce484caf3
NuttySalmon <nuttysalmon@gmail.com>
2019-05-17 17:27:32 -0700

    Created structs.

---
+++ concurrency.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+struct Customer{
+    char bring;
+    int fedCount;
+    bool gotH, gotF, gotS;
+};
+
+struct Food{
+    bool h, f, s;
+} food;
+
+
+void chef(){
+
+}
+
+int main(){
+    return 0;
+}
