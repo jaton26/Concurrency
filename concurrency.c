2b5664660bb258ab6751e96b53ee63436b5ddb07
NuttySalmon <nuttysalmon@gmail.com>
2019-05-17 17:59:01 -0700

    setup threads

--- concurrency.c
+++ concurrency.c
@@ -1,21 +1,47 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <pthread.h> 
 
-struct Customer{
+
+typedef struct{
     char bring;
     int fedCount;
     bool gotH, gotF, gotS;
-};
+    pthread_mutex_t *lock;
 
-struct Food{
+} Customer;
+
+typedef struct{
     bool h, f, s;
-} food;
+} Food;
 
 
-void chef(){
+void* chef(void *arg){
+    int counter = 0;
+    
+}
 
+void* eat(void *args){
+    Customer *cust = (Customer *) args;
+    
 }
 
 int main(){
+    Customer cust0, cust1, cust2;
+    cust0.bring = 'h';
+    cust1.bring = 'f';
+    cust2.bring = 's';
+
+    pthread_t eat0, eat1, eat2, cook;
+
+    pthread_create(&eat0, NULL, eat,  &cust0);
+    pthread_create(&eat1, NULL, eat,  &cust1);
+    pthread_create(&eat2, NULL, eat,  &cust2);
+    pthread_create(&cook, NULL, chef, NULL);
+
+    pthread_join(eat0, NULL);
+    pthread_join(eat1, NULL);
+    pthread_join(eat2, NULL);
+
     return 0;
 }
